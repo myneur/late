fcb552a3765d8e6a97d5f19db71c72eb947da000
Petr Meissner <myneur@Indrikobuk.local>
2019-02-10 23:35:42 +0100

    compatibility with music devices

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 monkeyc -o bin/late.prg -y ../developer_key.der -z resources/drawables/drawables.xml:resources/fonts/fonts.xml:resources/strings/strings.xml:resources-spa/strings/strings.xml:resources/settings/settings.xml:resources/settings/properties.xml -m manifest.xml source/*.mc -d fenix3_hr
 connectiq
-monkeydo bin/late.prg fenix3_hr
+monkeydo bin/late.prg vivoactive3m
